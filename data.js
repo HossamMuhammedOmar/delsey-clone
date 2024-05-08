230cbdf4a100b48a6f9200db81f40f8e50be3004
HossamMuhammedOmar <hossamomar9876@gmail.com>
2024-05-08 21:59:00 +0300

    nav, search, mega and animations done

---
+++ data.js
@@ -0,0 +1,25 @@
+export const NAV_LINKS = [
+  'Shipping and Returns',
+  'Track my Order',
+  'F.A.Q',
+  'Contact',
+];
+
+export const SOCIAL = [
+  {
+    title: 'instagram',
+    url: 'https://www.instagram.com/',
+  },
+  {
+    title: 'facebook',
+    url: 'https://www.facebook.com/',
+  },
+  {
+    title: 'pinterest',
+    url: 'https://www.pinterest.com/',
+  },
+  {
+    title: 'linkedin-in',
+    url: 'https://www.linkedin.com/',
+  },
+];
--- main.js
+++ main.js
@@ -1,24 +1,88 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { NAV_LINKS, SOCIAL } from './data';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const navList = document.querySelector('#nav-list');
+const socialList = document.querySelector('#social-list');
+const target = document.querySelector('#target-tag');
+const logoSection = document.querySelector('#logo-section');
+const searchInput = document.querySelector('#search');
+const logo = document.querySelector('#logo');
+const cardIcon = document.querySelector('#card-icon');
+const cardText = document.querySelector('#card-txt');
+const megaMenuWrapper = document.querySelector('#mega-menu-wrapper');
+const megaItems = document.querySelectorAll('.mega-item');
+const toggleMenuBtn = document.querySelector('#toggle-menu-btn');
 
-setupCounter(document.querySelector('#counter'))
+const items = [];
+const icons = [];
+let isMenuOpen = false;
+
+const obsever = new IntersectionObserver(entires =>
+  entires.forEach(entry => {
+    const cond = !entry.isIntersecting;
+
+    isMenuOpen = false;
+    logo.src = cond ? './assets/logoD.avif' : './assets/logo.avif';
+    cardIcon.src = cond ? './assets/cartD.svg' : './assets/cart.svg';
+    logoSection.classList.toggle('bg-white', cond);
+    cardText.classList.toggle('text-black', cond);
+
+    if (!isMenuOpen) {
+      megaMenuWrapper.classList.remove('!bg-white');
+      megaMenuWrapper.classList.remove('!translate-y-0');
+      // megaMenuWrapper.classList.remove('!opacity-100');
+    }
+
+    megaMenuWrapper.classList.toggle('bg-white', cond);
+    megaMenuWrapper.classList.toggle('translate-y-[-100%]', cond);
+    megaMenuWrapper.classList.toggle('opacity-0', cond);
+    megaItems.forEach(item => {
+      item.classList.toggle('!text-black', cond);
+    });
+
+    toggleMenuBtn.classList.toggle('!visible', cond);
+    toggleMenuBtn.classList.toggle('!translate-x-[-16px]', cond);
+
+    searchInput.classList.toggle('search-border', cond);
+    searchInput.classList.toggle('!translate-x-[-5px]', cond);
+  })
+);
+
+obsever.observe(target);
+
+toggleMenuBtn.addEventListener('click', () => {
+  isMenuOpen = !isMenuOpen;
+  megaMenuWrapper.classList.toggle('!bg-white', isMenuOpen);
+  megaMenuWrapper.classList.toggle('!translate-y-0', isMenuOpen);
+  megaMenuWrapper.classList.toggle('!opacity-100', isMenuOpen);
+});
+
+NAV_LINKS.forEach(link => {
+  const navItem = document.createElement('li');
+  const navLink = document.createElement('a');
+
+  navLink.innerHTML = link;
+  navLink.href = '#';
+  navLink.classList.add('link');
+  navItem.appendChild(navLink);
+  items.push(navItem);
+});
+
+SOCIAL.forEach(social => {
+  const listItem = document.createElement('li');
+  const socialLink = document.createElement('a');
+  const socialIcon = document.createElement('i');
+  const screenOnlyText = document.createElement('span');
+
+  socialIcon.classList.add('fa-brands', `fa-${social.title}`);
+
+  socialLink.href = social.url;
+  socialLink.target = '_blank';
+  screenOnlyText.innerHTML = social.title;
+  screenOnlyText.classList.add('sr-only');
+  socialLink.append(socialIcon, screenOnlyText);
+  listItem.appendChild(socialLink);
+  icons.push(listItem);
+});
+
+navList.append(...items);
+socialList.append(...icons);
